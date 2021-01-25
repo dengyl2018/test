f7b2edbfc028f98450f8757f73be88c2fbfe75d7
dengyongliang <dengyongliang@appledeMacBook-Pro.local>
2021-01-25 11:15:24 +0800

    test

--- paddlepaddle/layerfetch.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-from paddle import fluid
-x = fluid.layers.fill_constant(shape=[1], dtype='int64', value=5)
-y = fluid.layers.fill_constant(shape=[1], dtype='int64', value=1) 
-z = x + y
-exe = fluid.Executor(fluid.CPUPlace())
-result = exe.run(fluid.default_main_program(), fetch_list=[z])
-print result[0] # 6
